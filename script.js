0898c6732331d0868a1c5ab0357fe5223c3dd36f
ValijanovKolyanich <kingdomking.uz@gmail.com>
2022-06-24 06:07:57 +0500

    upgraded

--- script.js
+++ script.js
@@ -11,18 +11,21 @@ button.onclick = start;
 function start() {
   const startTime = Date.now();
 
-  display.textContent = TIMEOUT;
   button.onclick = () => counter.textContent = clicks++;
 
   const interval = setInterval(() => {
-    const delta = date.now() - startTime;
-    display.textContent = TIMEOUT - delta;
-  });
+    const Delta = Date.now() - startTime;
+    display.textContent = TIMEOUT - Delta;
+  }, 100);
 
   const timeout = setTimeout(() => {
     button.onclick = 0;
-   
+   display.textContent = `Game over`;
 
+    clearInterval(interval)
     clearTimeout(timeout);
   }, TIMEOUT);
+}
+function formatTime(ms) {
+  return Number.parseFloat(ms/1000).toFixed(2);
 }
